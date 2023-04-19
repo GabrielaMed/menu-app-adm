f79932c44ce2114a3ef1c36ac42fddf07749df25
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-04-19 16:28:19 -0400

    rmv user homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import { NewProduct } from '../pages/NewProduct';
 import { CompanyHome } from '../pages/CompanyHome';
 import { ProductList } from '../pages/ProductList';
-import { Home } from '../pages/Home';
 
 export const AppRoutes = () => (
   <Routes>
@@ -10,6 +9,5 @@ export const AppRoutes = () => (
     <Route path='/:companyId/products' element={<ProductList />} />
     <Route path='/:companyId/product' element={<NewProduct />} />
     <Route path='/:companyId/product/:productId' element={<NewProduct />} />
-    <Route path='/' element={<Home />} />
   </Routes>
 );
