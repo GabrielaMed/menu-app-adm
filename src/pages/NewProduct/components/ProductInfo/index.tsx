854822eaf5d187e1f19c81eed8856001911c763f
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-06-07 17:04:10 -0400

    add loading and delete image

--- src/pages/NewProduct/components/ProductInfo/index.tsx
+++ src/pages/NewProduct/components/ProductInfo/index.tsx
@@ -5,13 +5,15 @@ import Button from 'react-bootstrap/Button';
 import { ProductSchema } from '../../../../validation/productValidation';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { api } from '../../../../services/api';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { IToastType } from '../../../../utils/Interface/Toast';
 import { ToastMessage } from '../../../../components/Toast';
 import { AxiosError } from 'axios';
 import { productImageRegistration } from '../../../../utils/productImageRegistration';
 import { useNavigate } from 'react-router-dom';
 import { GlobalContext } from '../../../../shared/GlobalContext';
+import { MdDelete } from 'react-icons/md';
+import ReactLoading from 'react-loading';
 
 interface Props {
   productData: IProduct;
@@ -33,7 +35,9 @@ export const ProductInfo = ({ productData, setProductData }: Props) => {
   );
   const [toastMessage, setToastMessage] = useState('');
   const navigate = useNavigate();
-  const { companyId, productId } = useContext(GlobalContext);
+  const { companyId, productId, setProductId } = useContext(GlobalContext);
+  const [relateImage, setRelateImage] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const uploadImages = (files: any) => {
     let formData = new FormData();
@@ -44,13 +48,18 @@ export const ProductInfo = ({ productData, setProductData }: Props) => {
 
     setProductData((state: any) => ({
       ...state,
-      image: formData.getAll('file'),
+      Image: formData.getAll('file'),
     }));
+
+    if (productId) {
+      setRelateImage(true);
+    }
   };
 
-  async function handleRegister(data: any) {
+  const handleRegister = async (data: any) => {
     if (!productId) {
       try {
+        setLoading(true);
         const response = await api.post(`${companyId}/product`, {
           name: data.name,
           description: data.description,
@@ -58,17 +67,17 @@ export const ProductInfo = ({ productData, setProductData }: Props) => {
         });
 
         if (response?.data?.product) {
-          await productImageRegistration.handle(
-            response?.data?.product?.id,
-            productData.Image
-          );
+          setProductId(response?.data?.product?.id);
+          setRelateImage(true);
+          setLoading(false);
 
           setShowToast(true);
           setToastMessageType(IToastType.success);
           setToastMessage('Produto criado!');
 
+          setProductId(response?.data?.product?.id);
           setTimeout(() => {
-            navigate(`/${companyId}/product/${response?.data?.product?.id}`);
+            navigate(`/product`);
           }, 5000);
         }
       } catch (err) {
@@ -91,21 +100,23 @@ export const ProductInfo = ({ productData, setProductData }: Props) => {
       }));
     } else {
       try {
+        setLoading(true);
         const response = await api.put(`${companyId}/product/${productId}`, {
           name: data.name,
           description: data.description,
           price: data.price,
         });
-
         if (response?.data?.product) {
-          await productImageRegistration.handle(
-            response?.data?.product?.id,
-            productData.Image
-          );
+          setLoading(false);
 
           setShowToast(true);
           setToastMessageType(IToastType.success);
           setToastMessage('Produto atualizado!');
+
+          setProductId(response?.data?.product?.id);
+          setTimeout(() => {
+            navigate(`/product`);
+          }, 5000);
         }
       } catch (err) {
         if (err instanceof AxiosError) {
@@ -119,7 +130,61 @@ export const ProductInfo = ({ productData, setProductData }: Props) => {
         }
       }
     }
-  }
+  };
+
+  const deleteImage = async (imageId: string) => {
+    try {
+      const response = await api.delete(`image/${imageId}`);
+
+      if (response.status === 200) {
+        const updatedProductData = {
+          ...productData,
+          Image: productData?.Image?.filter((item) => item.id !== imageId),
+        };
+
+        setProductData(updatedProductData);
+
+        setShowToast(true);
+        setToastMessageType(IToastType.success);
+        setToastMessage('Imagem removida com sucesso.');
+      }
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        setShowToast(true);
+        setToastMessageType(IToastType.error);
+        setToastMessage(`Error: ${err?.response?.data}`);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const handleRelateImage = async () => {
+      setLoading(true);
+      const response: any = await productImageRegistration.handle(
+        productId,
+        productData.Image
+      );
+      setRelateImage(false);
+
+      if (response?.data?.status === 'success' && response?.data?.fileName) {
+        setProductData((state: IProduct) => ({
+          ...state,
+          name: response.data.product.name,
+          Image: [
+            {
+              fileName: response.data.fileName,
+              id: response.data.id,
+            },
+          ],
+        }));
+      }
+    };
+
+    if (productData && productId && relateImage === true) {
+      handleRelateImage();
+      setLoading(false);
+    }
+  }, [relateImage]);
 
   return (
     <>
@@ -129,99 +194,121 @@ export const ProductInfo = ({ productData, setProductData }: Props) => {
         toastMessage={toastMessage}
         toastMessageType={toastMessageType}
       />
-      <Form onSubmit={handleSubmit(handleRegister)}>
-        <Form.Group className='mb-3'>
-          <Form.Label>Foto:</Form.Label>
-          <Form.Control
-            type='file'
-            placeholder='Insira uma foto'
-            accept='image/png, image/jpeg, image/pjpeg'
-            multiple
-            required
-            {...register('image')}
-            onChange={(e) =>
-              uploadImages((e.target as HTMLInputElement).files!)
-            }
+      {loading && (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <ReactLoading
+            type={'cylon'}
+            color={'#4B2995'}
+            height={'150px'}
+            width={'150px'}
           />
-        </Form.Group>
+        </div>
+      )}
+      {!loading && (
+        <Form onSubmit={handleSubmit(handleRegister)}>
+          <Form.Group className='mb-3'>
+            <Form.Label>Foto:</Form.Label>
+            <Form.Control
+              type='file'
+              placeholder='Insira uma foto'
+              accept='image/png, image/jpeg, image/pjpeg'
+              multiple
+              required
+              {...register('image')}
+              onChange={(e) =>
+                uploadImages((e.target as HTMLInputElement).files!)
+              }
+            />
+          </Form.Group>
 
-        <Carousel style={{ background: 'black', marginBottom: '1rem' }}>
-          {productData?.Image?.map((item, idx) => {
-            return (
-              <Carousel.Item key={idx}>
-                <img
-                  className='d-block w-100'
-                  style={{ objectFit: 'cover', height: '15rem' }}
-                  src={process.env.REACT_APP_IMAGE_URL + item?.fileName}
-                  alt=''
-                />
-              </Carousel.Item>
-            );
-          })}
-        </Carousel>
+          <Carousel style={{ background: 'black', marginBottom: '1rem' }}>
+            {productData?.Image
+              ? productData?.Image?.map((item, idx) => {
+                  return (
+                    <Carousel.Item key={idx}>
+                      <img
+                        className='d-block w-100'
+                        style={{ objectFit: 'cover', height: '15rem' }}
+                        src={
+                          process.env.REACT_APP_IMAGE_URL + item?.fileName ||
+                          item.fileName
+                        }
+                        alt={item.fileName}
+                      />
+                      <MdDelete
+                        size={24}
+                        className='delete-icon'
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => deleteImage(item.id)}
+                      />
+                    </Carousel.Item>
+                  );
+                })
+              : null}
+          </Carousel>
+          <Form.Group className='mb-3'>
+            <Form.Label>Nome:</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Nome do produto'
+              required
+              defaultValue={productData?.name}
+              {...register('name')}
+            />
+            {errors.name && (
+              <span style={{ color: 'red', marginTop: '0.5rem' }}>
+                {errors.name?.message?.toString()}
+              </span>
+            )}
+          </Form.Group>
 
-        <Form.Group className='mb-3'>
-          <Form.Label>Nome:</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Nome do produto'
-            required
-            defaultValue={productData?.name}
-            {...register('name')}
-          />
-          {errors.name && (
-            <span style={{ color: 'red', marginTop: '0.5rem' }}>
-              {errors.name?.message?.toString()}
-            </span>
-          )}
-        </Form.Group>
+          <Form.Group className='mb-3'>
+            <Form.Label>Descrição:</Form.Label>
+            <Form.Control
+              as='textarea'
+              style={{ resize: 'none' }}
+              rows={3}
+              placeholder='Descrição'
+              required
+              defaultValue={productData?.description}
+              {...register('description')}
+            />
+            {errors.description && (
+              <span style={{ color: 'red', marginTop: '0.5rem' }}>
+                {errors?.description?.message?.toString()}
+              </span>
+            )}
+          </Form.Group>
+          <Form.Group className='mb-3'>
+            <Form.Label>Valor:</Form.Label>
+            <Form.Control
+              type='number'
+              placeholder='Valor'
+              step='0.01'
+              min='0'
+              required
+              defaultValue={productData?.price}
+              {...register('price')}
+            />
+            {errors.price && (
+              <span style={{ color: 'red', marginTop: '0.5rem' }}>
+                {errors?.price?.message?.toString()}
+              </span>
+            )}
+          </Form.Group>
 
-        <Form.Group className='mb-3'>
-          <Form.Label>Descrição:</Form.Label>
-          <Form.Control
-            as='textarea'
-            style={{ resize: 'none' }}
-            rows={3}
-            placeholder='Descrição'
-            required
-            defaultValue={productData?.description}
-            {...register('description')}
-          />
-          {errors.description && (
-            <span style={{ color: 'red', marginTop: '0.5rem' }}>
-              {errors?.description?.message?.toString()}
-            </span>
-          )}
-        </Form.Group>
-        <Form.Group className='mb-3'>
-          <Form.Label>Valor:</Form.Label>
-          <Form.Control
-            type='number'
-            placeholder='Valor'
-            step='0.01'
-            min='0'
-            required
-            defaultValue={productData?.price}
-            {...register('price')}
-          />
-          {errors.price && (
-            <span style={{ color: 'red', marginTop: '0.5rem' }}>
-              {errors?.price?.message?.toString()}
-            </span>
-          )}
-        </Form.Group>
-
-        <Button
-          style={{
-            backgroundColor: '#4B2995',
-            border: '1px  solid transparent',
-          }}
-          type='submit'
-          disabled={isSubmitting}
-        >
-          {productId ? 'Salvar' : 'Criar'}
-        </Button>
-      </Form>
+          <Button
+            style={{
+              backgroundColor: '#4B2995',
+              border: '1px  solid transparent',
+            }}
+            type='submit'
+            disabled={isSubmitting}
+          >
+            {productId ? 'Salvar' : 'Criar'}
+          </Button>
+        </Form>
+      )}
     </>
   );
 };
