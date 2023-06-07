aed2f6e15853e510cccc3924faecadede953c9d6
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-06-07 17:04:23 -0400

    add try catch

--- src/utils/productImageRegistration/index.ts
+++ src/utils/productImageRegistration/index.ts
@@ -3,30 +3,35 @@ import { IProduct } from '../Interface/Product';
 
 class ProductImageRegistration {
   async handle(productId: string, images: IProduct['Image']) {
-    if (!images) return;
-    const config = {
-      headers: { 'Content-Type': 'multipart/form-data' },
-    };
-    let response;
+    try {
+      if (!images) return;
+      const config = {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      };
 
-    const uploadImage = async (index: number) => {
-      if (!images[index]) return;
+      const uploadImage = async (index: number): Promise<any> => {
+        if (!images[index]) return;
 
-      response = await api.post(
-        `product/${productId}/image`,
-        {
-          file: images[index],
-        },
-        config
-      );
+        const response = await api.post(
+          `product/${productId}/image`,
+          {
+            file: images[index],
+          },
+          config
+        );
 
-      console.log(response, 'img resp');
-      await uploadImage(index + 1);
-    };
+        if (index + 1 < images.length) {
+          await uploadImage(index + 1);
+        }
 
-    await uploadImage(0);
+        return response;
+      };
 
-    return response;
+      const response = await uploadImage(0);
+      return response;
+    } catch (err) {
+      console.log('error', err);
+    }
   }
 }
 
